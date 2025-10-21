2c4264c5d9e2210f2f4b4723654eea2ca4805c87
Anh Nguyen Hoang <anhnh@ikameglobal.com>
2025-10-21 06:19:29 +0000

    Hãy fix chiều dài màn hình phù hợp với từng điện thoại. Còn chiều ngang

--- src/app/(game)/layout.tsx
+++ src/app/(game)/layout.tsx
@@ -1,7 +1,7 @@
 export default function GameLayout({children}: {children: React.ReactNode}) {
   return (
     <div className="flex h-screen w-full items-center justify-center bg-zinc-700">
-      <main className="relative flex h-full max-h-dvh w-full max-w-[392px] flex-col overflow-hidden rounded-2xl border-4 border-black bg-slate-900 shadow-2xl sm:border-8 sm:aspect-[3/4] sm:h-auto sm:max-h-[95dvh]">
+      <main className="relative flex aspect-[3/4] h-dvh max-w-full flex-col overflow-hidden bg-slate-900 shadow-2xl sm:h-auto sm:max-h-[95dvh] sm:max-w-[392px] sm:rounded-2xl sm:border-8 sm:border-black">
         {children}
       </main>
     </div>
